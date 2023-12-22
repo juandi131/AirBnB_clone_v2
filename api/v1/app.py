82324eeeca35cd3b64b49814dc699d2cb012556b
juandi131 <juandigarcia3@gmail.com>
2023-12-22 10:15:04 -0800

    12

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,6 +15,13 @@ def teardown_appcontext(exception):
     """Method that call storage.close"""
     storage.close()
 
+
+@app.errorhandler(404)
+def not_found(error):
+    """Error not found 404 with json"""
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = os.getenv("HBNB_API_PORT", "5000")
