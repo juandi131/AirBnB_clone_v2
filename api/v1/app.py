6ea17480c32ab0f38695fc953344d82d373ff9d5
juandi131 <juandigarcia3@gmail.com>
2023-12-22 09:54:25 -0800

    12

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-from flask import Flask
+"""Start API!"""
+
+import os
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-import os
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
+
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """Method that call storage.close"""
     storage.close()
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     host = os.getenv("HBNB_API_HOST", "0.0.0.0")
     port = os.getenv("HBNB_API_PORT", "5000")
     app.run(host=host, port=port, threaded=True)
